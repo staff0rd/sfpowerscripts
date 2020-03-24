3a5f9d5c10db0b59249386b20807b94ec5c50dda
Azlam Abdulsalam <azlam.salam@outlook.com>
2020-03-24 20:48:46 +1100

    Feature/apex test (#118)

--- BuildTasks/TriggerApexTestTask/TriggerApexTest.ts
+++ BuildTasks/TriggerApexTestTask/TriggerApexTest.ts
@@ -9,28 +9,26 @@ async function run() {
   let test_options = {};
 
   try {
-    
-    AppInsights.setupAppInsights(tl.getBoolInput("isTelemetryEnabled",true));
+    AppInsights.setupAppInsights(tl.getBoolInput("isTelemetryEnabled", true));
     const target_org: string = tl.getInput("target_org", true);
-  
+
     test_options["wait_time"] = tl.getInput("wait_time", true);
 
+   
     test_options["testlevel"] = tl.getInput("testlevel", true);
     test_options["synchronous"] = tl.getBoolInput("synchronous", false);
 
-
-
     if (test_options["testlevel"] == "RunSpecifiedTests")
-    test_options["specified_tests"] = tl.getInput("specified_tests", true);
+      test_options["specified_tests"] = tl.getInput("specified_tests", true);
     if (test_options["testlevel"] == "RunApexTestSuite")
-    test_options["apextestsuite"] = tl.getInput("apextestsuite", true);
-   
+      test_options["apextestsuite"] = tl.getInput("apextestsuite", true);
 
     let taskType = tl.getVariable("Release.ReleaseId") ? "Release" : "Build";
     let stagingDir: string = "";
+
     if (taskType == "Build") {
       stagingDir = path.join(
-        tl.getVariable("build.artifactStagingDirectory"),
+        tl.getVariable("build.artifactStagingDirectory")?tl.getVariable("build.artifactStagingDirectory"):"staging",
         ".testresults"
       );
 
@@ -40,23 +38,28 @@ async function run() {
       console.log(stagingDir);
     }
 
+    test_options["outputdir"] = stagingDir;
 
- 
-
-    test_options['outputdir']=stagingDir;
-
-
-    let triggerApexTestImpl:TriggerApexTestImpl = new TriggerApexTestImpl(target_org,test_options);
+    let triggerApexTestImpl: TriggerApexTestImpl = new TriggerApexTestImpl(
+      target_org,
+      test_options
+    );
     console.log("Executing command");
-    await triggerApexTestImpl.exec();
+    let result = await triggerApexTestImpl.exec();
 
-  
+    if (!result.result) {
+      tl.setResult(tl.TaskResult.Failed, result.message);
+    } else {
+      tl.setResult(tl.TaskResult.Succeeded, result.message);
+    }
 
     AppInsights.trackTask("sfpwowerscript-triggerapextest-task");
-    AppInsights.trackTaskEvent("sfpwowerscript-triggerapextest-task","apex_test_triggered");    
-
+    AppInsights.trackTaskEvent(
+      "sfpwowerscript-triggerapextest-task",
+      "apex_test_triggered"
+    );
   } catch (err) {
-    AppInsights.trackExcepiton("sfpwowerscript-triggerapextest-task",err);    
+    AppInsights.trackExcepiton("sfpwowerscript-triggerapextest-task", err);
     tl.setResult(tl.TaskResult.Failed, err.message);
   }
 }
--- BuildTasks/TriggerApexTestTask/TriggerApexTestImpl.ts
+++ BuildTasks/TriggerApexTestTask/TriggerApexTestImpl.ts
@@ -1,25 +1,99 @@
 import child_process = require("child_process");
 import { onExit } from "../Common/OnExit";
+import { fail } from "assert";
+import { isNullOrUndefined } from "util";
+let fs = require("fs-extra");
+let path = require("path");
 
 export default class TriggerApexTestImpl {
   public constructor(private target_org: string, private test_options: any) {}
 
-  public async exec(): Promise<void> {
-    
-    //Print Test in Human Reable Format and also store it in staging directory
-    let child = child_process.exec(
-      this.buildExecCommand(),
-      { maxBuffer: 1024 * 1024*5,encoding: "utf8" },
-      (error, stdout, stderr) => {
-        if (error) throw error;
+  public async exec(): Promise<{
+    id: string;
+    result: boolean;
+    message: string;
+  }> {
+
+
+    let test_result: { id: string; result: boolean; message: string } = {
+      id: "",
+      result: false,
+      message: ""
+    };
+
+    let output = "";
+    let error = ""
+    try {
+      //Print final output
+      let child = child_process.exec(
+        this.buildExecCommand(),
+        {
+          maxBuffer: 1024 * 1024 * 5,
+          encoding: "utf8"
+        },
+        (error, stdout, stderr) => {}
+      );
+
+      child.stdout.on("data", data => {
+        output += data.toString();
+      });
+      child.stderr.on("data", data => {
+        error += data.toString();
+      });
+
+      await onExit(child);
+    } catch (err) {
+   
+    }
+
+    console.log(output);
+
+    try {
+      let test_id = fs
+        .readFileSync(
+          path.join(this.test_options["outputdir"], "test-run-id.txt")
+        )
+        .toString();
+
+      console.log('test_id',test_id);
+
+      let test_report_json_file = fs
+        .readFileSync(
+          path.join(
+            this.test_options["outputdir"],
+            `test-result-${test_id}.json`
+          )
+        )
+        .toString();
+
+      let test_report_json = JSON.parse(test_report_json_file);
+
+      test_result.id = test_id;
+
+      //Print human readable output to console
+      if ((test_report_json.summary.outcome = "Failed")) {
+        test_result.message = `${test_report_json.summary.failing} Tests failed with overall Test Run Coverage of ${test_report_json.summary.testRunCoverage}`;
+        test_result.message += "\nFailed Test Cases:";
+
+        test_report_json.tests.forEach(element => {
+          if (element.Outcome == "Fail") {
+            test_result.message += "\n"+ `${element.MethodName}  ${element.Message}    ${element.StackTrace}`;
+          }
+        });
+        test_result.result = false;
+        console.error(output);
+      } else {
+        test_result.message = `${test_report_json.summary.passing} Tests passed with overall Test Run Coverage of ${test_report_json.summary.testRunCoverage}`;
+        test_result.result = true;
+        console.log(output);
       }
-    );
 
-    child.stdout.on("data", data => {
-      console.log(data.toString());
-    });
-
-    await onExit(child);
+      return test_result;
+    } catch (err) {
+      test_result.result = false;
+      test_result.message = error;
+      return test_result;
+    }
   }
 
   private buildExecCommand(): string {
@@ -42,15 +116,13 @@ export default class TriggerApexTestImpl {
       command += ` -l ${this.test_options["testlevel"]}`;
     }
 
-
     if (this.test_options["testlevel"] == "RunSpecifiedTests") {
       command += ` -t ${this.test_options["specified_tests"]}`;
     } else if (this.test_options["testlevel"] == "RunApexTestSuite") {
       command += ` -s ${this.test_options["apextestsuite"]}`;
     }
+
     console.log(`Generated Command: ${command}`);
     return command;
   }
-
-
 }
